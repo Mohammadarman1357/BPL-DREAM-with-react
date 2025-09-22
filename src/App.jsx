998c7cf61f0e60f88e5e4ae76882580f9e8889e6
Arman <mohammadjahedulislamarman@gmail.com>
2025-09-23 01:29:55 +0600

    added toggle condition by using usestate

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import AvailablePlayers from './components/AvailablePlayers/AvailablePlayers';
 import SelectedPlayers from './components/SelectedPlayers/SelectedPlayers';
 import Navbar from './components/Navbar/Navbar';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 
 const fetchPlayers = async () => {
   const res = await fetch("/player.json")
@@ -11,15 +11,28 @@ const fetchPlayers = async () => {
 
 function App() {
 
+  const [toggle, setToggle] = useState(true);
   const playerPromise = fetchPlayers();
   return (
     <>
       <Navbar></Navbar>
-      <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
 
-        <AvailablePlayers playerPromise={playerPromise}></AvailablePlayers>
-        {/* <SelectedPlayers></SelectedPlayers> */}
-      </Suspense>
+      <div className='max-w-[1200px] mx-auto flex justify-between items-center sora-font'>
+        <h1 className='font-bold text-2xl'>Available Players</h1>
+
+        <div>
+
+          <button onClick={() => setToggle(true)} className={`px-6 py-3 border-1 border-r-0 border-gray-400 rounded-l-2xl font-bold ${toggle === true ? "bg-[#e7fe29]" : "bg-none"}`}>Available</button>
+          <button onClick={() => setToggle(false)} className={`px-6 py-3 border-1 border-l-0 border-gray-400 rounded-r-2xl font-bold ${toggle === false ? "bg-[#e7fe29]" : "bg-none"}`}>Selected <span>(0)</span></button>
+
+        </div>
+      </div>
+
+      {
+        toggle === true ? <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
+          <AvailablePlayers playerPromise={playerPromise}></AvailablePlayers>
+        </Suspense> : <SelectedPlayers></SelectedPlayers>
+      }
 
     </>
   )
